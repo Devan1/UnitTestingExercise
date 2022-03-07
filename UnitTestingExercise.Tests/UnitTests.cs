9d39becfb92179c570cd014882ccb0cb4b4aba96
Devan <deshlock010101@hotmai.com>
2022-03-07 15:08:40 -0600

    completed exercise

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,73 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1,2,3,6)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var testAdd = new UnitTestMethods();
             //Act
-
+            var actual = testAdd.add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10,2,8)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
-
+            var testSubtract = new UnitTestMethods();
             //Act
-
+            var actual = testSubtract.subract(minuend, subtrhend);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10,2,20)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            var testMultiply = new UnitTestMethods();
             //Act
-
+            var actual = testMultiply.multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10,2,5)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            var testDivide = new UnitTestMethods();
             //Act
-
+            var actual = testDivide.divide(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void YourMethodName()
+        public void Factmul()
         {
             //Arrange
-
+            var testFactmul = new UnitTestMethods();
             //Act
-
+            var actual = testFactmul.factmul();
             //Assert
-
+            Assert.Equal(6, actual);
         }
 
         [Fact]
-        public void YourMethodName2()
+        public void Factdiv()
         {
             //Arrange
-
+            var testFactdiv = new UnitTestMethods();
             //Act
-
+            var actual = testFactdiv.factdiv();
             //Assert
+            Assert.Equal(2, actual);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -4,23 +4,47 @@ namespace UnitTestingExercise
     public class UnitTestMethods
     {
         // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // Righ-click on Dependencies in your .Tests project and set your reference
 
 
 
         // Create an Add method that passes 3 integers
-
+        public int add(int num1, int num2, int num3)
+        {
+            //throw new NotImplementedException(); 
+            return num1 + num2 + num3;
+        }
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
-
+        // Keep track of which number is getting passed as minuend and subtrahend
+        public int subract(int minuend, int subtrahend)
+        {
+            //throw new NotImplementedException();
+            return minuend - subtrahend;
+        }
         // Create a Multiply method that passes 2 integers
+        public int multiply(int num1, int num2)
+        {
+            //throw new NotImplementedException();
+            return num1 * num2;
+        }
 
         // Create a Divide method that passes 2 integers
 
-
+        public int divide(int num1, int num2)
+        {
+            //throw new NotImplementedException();
+            return num1 / num2;
+        }
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
-
+        public int factmul()
+        {
+            return 2 * 3;
+        }
+        public int factdiv()
+        {
+            return 6 / 3;
+        }
         
     }
 }
